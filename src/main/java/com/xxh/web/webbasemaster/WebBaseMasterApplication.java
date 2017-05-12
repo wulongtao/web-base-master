4e1d64d2880392988ba27221638f7f9336e8a612
wulongtao <769418278@qq.com>
2017-05-12 13:41:29 +0800

    a quick developing base project using ssm framework

---
+++ src/main/java/com/xxh/web/webbasemaster/WebBaseMasterApplication.java
@@ -0,0 +1,12 @@
+package com.xxh.web.webbasemaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBaseMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBaseMasterApplication.class, args);
+	}
+}
